e9b0df4de6d2f1c8046090c46f937d35254acb1e
Qinghuan Liu <qinghuan.liu@globetech.com>
2026-05-13 10:47:30 +0200

    feat: add the mempool.c and implementations

---
+++ src/mempool.c
@@ -0,0 +1,33 @@
+#include "mempool.h"
+
+int mempool_init(mempool_t* mempool)
+{
+    for (int i = 0; i < mempool->block_count; i++)
+    {
+        node_t* node = (node_t*)(mempool->buffer + mempool->block_size * i);
+        if (i == mempool->block_count - 1)
+        {
+            node->next = NULL;
+        }
+        else
+        {
+            node->next = (node_t*)(mempool->buffer + mempool->block_size * (i + 1));
+        }
+    }
+    mempool->free_head = (node_t*)mempool->buffer;
+    return 0;
+}
+
+void* mempool_alloc(mempool_t* mempool)
+{
+    node_t* free_head  = mempool->free_head;
+    mempool->free_head = free_head->next;
+    return free_head;
+}
+
+void mempool_free(mempool_t* mempool, void* head)
+{
+    node_t* block      = (node_t*)head;
+    block->next        = mempool->free_head;
+    mempool->free_head = block;
+}
