c7222efac79c9c4fe0ca45af1ae20d6b68d42f27
Qinghuan Liu <qinghuan.liu@globetech.com>
2026-05-13 11:13:41 +0200

    test: add test features

--- test/test_mempool.c
+++ test/test_mempool.c
@@ -1,4 +1,30 @@
+#include "mempool.h"
+#include "unity.h"
+
+#define BLOCKSIZE  16
+#define BLOCKCOUNT 3
+
+void setUp(void)
+{
+}
+void tearDown(void)
+{
+}
+
+void test_init(void)
+{
+    static uint8_t buffer[BLOCKSIZE * BLOCKCOUNT];
+    mempool_t      mempool;
+    mempool.block_count = BLOCKCOUNT;
+    mempool.block_size  = BLOCKSIZE;
+    mempool.buffer      = buffer;
+    mempool_init(&mempool);
+    TEST_ASSERT_NOT_NULL(mempool.free_head);
+}
+
 int main(void)
 {
-    return 0;
+    UNITY_BEGIN();
+    RUN_TEST(test_init);
+    return UNITY_END();
 }
